339e5e8acdd21a9ec00ebadd03cbd5841491ce16
Jeff Chiu <jwchiu82@gmail.com>
2021-01-22 09:04:55 -0800

    add context file and dog & cat models

---
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Models
+{
+    public class AnimalShelterContext : DbContext
+    {
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Cat> Cats { get; set; }
+        public DbSet<Dog> Dogs { get; set; }
+    }
+}
