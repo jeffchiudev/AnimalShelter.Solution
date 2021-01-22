7216ec54e5aa6f1c8ba0ac528ea99e6d4791a7a4
Jeff Chiu <jwchiu82@gmail.com>
2021-01-22 12:19:37 -0800

    add validations to models

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
     public class Animal
     {
         public int AnimalId { get; set; }
+        [Required]
         public string AnimalName { get; set; }
+        [Required]
         public string Species { get; set; }
         public string Breed { get; set; }
+        [Required]
         public string Gender { get; set; }
         public int Age { get; set; }
     }
--- AnimalShelter/Models/Foster.cs
+++ AnimalShelter/Models/Foster.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
     public class Foster
     {
         public int FosterId { get; set; }
+        [Required]
         public string FosterName { get; set; }
+        [Required]
         public string FosterEmail { get; set; } //contact info
         public bool FosterAvailability { get; set; } //is foster open for fostering
     }
